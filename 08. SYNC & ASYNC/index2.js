0fdfdc347a04b2158992ab9be933052cd1b85eaf
MOHAMMAD JUNAID <junnu2757@gmail.com>
2025-01-29 13:47:59 -0800

    update😉

--- 08. SYNC & ASYNC/index2.js
+++ 08. SYNC & ASYNC/index2.js
@@ -1,19 +1,52 @@
+// const http = require('http')
+// const fs = require('fs')
+
+// const asyncServer = http.createServer((req,res)=>{
+//     if(req.url==='/'){
+//         fs.readFile('example.txt','utf8',(err,data)=>{
+//             if(err){
+//                 res.statusCode=500;
+//                 return res.end('Error for reading file')
+//             }
+//             res.end(data)
+//         })
+//     }
+   
+// })
+// asyncServer.listen(7000,()=>{console.log('server start')})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const http = require('http')
 const fs = require('fs')
 
-const asyncServer = http.createServer((req,res)=>{
-    if(req.url==='/'){
-        fs.readFile('example.txt','utf8',(err,data)=>{
+const sync = http.createServer((req,res)=>{
+    if(req.url === '/')
+    {
+        const data = fs.readFile('example.txt','utf8',(err,data)=>{
             if(err){
-                res.statusCode=500;
-                return res.end('Error for reading file')
+                res.statusCode =500
+                return  res.end('Error aaya hai')
             }
-            res.end(data)
+             res.end(data)
         })
     }
-   
 })
-asyncServer.listen(7000,()=>{console.log('server start')})
-
-
-
+sync.listen(8000,()=>{console.log("localhost:8000")})
